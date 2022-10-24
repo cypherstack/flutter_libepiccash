275c2aab7acd515194c2a9afe95b39542dfc8c59
sneurlax <sneurlax@gmail.com>
2022-10-24 18:15:05 -0500

    openssl workaround and change target to x86_64-pc-windows-msvc

--- scripts/windows/build_all.ps1
+++ scripts/windows/build_all.ps1
@@ -1,6 +1,17 @@
 # !/bin/pwsh
 
-rustup target add x86_64-pc-windows-gnu
+rustup target add x86_64-pc-windows-msvc
+
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+.\bootstrap-vcpkg.bat
+.\vcpkg.exe install openssl:x64-windows-static
+cd ..
+
+$env:OPENSSL_DIR = "${pwd}\installed\x64-windows-static"
+$env:OPENSSL_STATIC = 'Yes'
+[System.Environment]::SetEnvironmentVariable('OPENSSL_DIR', $env:OPENSSL_DIR, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('OPENSSL_STATIC', $env:OPENSSL_STATIC, [System.EnvironmentVariableTarget]::User)
 
 New-Item -ItemType Directory -Force -Path build
 $env:COMMIT = $(git log -1 --pretty=format:"%H")
@@ -13,14 +24,15 @@ Copy-Item "..\..\rust\*" -Destination "build\rust" -Force -Recurse
 cd build\rust
 if (Test-Path 'env:IS_ARM ') {
     Write-Output "Building arm version"
-    cargo build --target aarch64-pc-windows-gnu --release --lib
+    cargo build --target aarch64-pc-windows-msvc --release --lib
 
-    New-Item -ItemType Directory -Force -Path target\aarch64-pc-windows-gnu\release
-    Copy-Item "target\aarch64-pc-windows-gnu\release\libepic_cash_wallet.so" -Destination "target\aarch64-pc-windows-gnu\release\" -Force
+    New-Item -ItemType Directory -Force -Path target\aarch64-pc-windows-msvc\release
+    Copy-Item "target\aarch64-pc-windows-msvc\release\libepic_cash_wallet.so" -Destination "target\aarch64-pc-windows-gnu\release\" -Force
 } else {
     Write-Output "Building x86_64 version"
-    New-Item -ItemType Directory -Force -Path target\x86_64-pc-windows-gnu\release
-    cargo build --target x86_64-pc-windows-gnu --release --lib
+    New-Item -ItemType Directory -Force -Path target\x86_64-pc-windows-msvc\release
+
+    cargo build --target x86_64-pc-windows-msvc --release --lib
 }
 
 # Return to /scripts/windows
