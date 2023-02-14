2d5f87a74d530412b79af9142f4492aaa29a6960
sneurlax <sneurlax@gmail.com>
2023-02-14 09:47:32 -0600

    remove unused import

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -145,7 +145,6 @@ use log::Level;
 use android_logger::Config as AndroidConfig;
 use ffi_helpers::{export_task, Task};
 use ffi_helpers::task::{CancellationToken};
-use serde_json::json;
 use stack_epic_wallet_libwallet::api_impl::owner::get_public_address;
 use stack_test_epicboxlib::utils::crypto::{Hex, sign_challenge};
 
