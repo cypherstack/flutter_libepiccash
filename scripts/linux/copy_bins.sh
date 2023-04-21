336d11439cd9d723a282525bff8a67f472e3d6cd
sneurlax <sneurlax@gmail.com>
2023-04-21 16:35:24 -0500

    @julian-CStack's update to linux copy_bins.sh script

--- scripts/linux/copy_bins.sh
+++ scripts/linux/copy_bins.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Failed to copy lib bins. Missing target root dir path"
-    exit 1
-fi
+OS=linux
+TAG_COMMIT=$(git log -1 --pretty=format:"%H")
 
-
-TARGET_PATH=build/rust/target
-
-ARM_BIN=aarch64-unknown-linux-gnu/release/libepic_cash_wallet.so
-
-if [ -f "$TARGET_PATH/$ARM_BIN" ]; then
-  cp "$TARGET_PATH/$ARM_BIN" "$1"/linux/bin/"$ARM_BIN"
+rm -rf flutter_libepiccash_bins
+git clone https://git.cypherstack.com/stackwallet/flutter_libepiccash_bins
+if [ -d flutter_libepiccash_bins ]; then
+  cd flutter_libepiccash_bins
 else
-  echo "$ARM_BIN not found!"
+  echo "Failed to clone flutter_libepiccash_bins"
+  exit 1
 fi
 
+TARGET_PATH=../build/rust/target
+BIN=libepic_cash_wallet.so
 
-X86_64_BIN=x86_64-unknown-linux-gnu/release/libepic_cash_wallet.so
+for TARGET in aarch64-unknown-linux-gnu x86_64-unknown-linux-gnu
+do
+  ARCH_PATH=$TARGET/release
 
-if [ -f "$TARGET_PATH/$X86_64_BIN" ]; then
-  cp "$TARGET_PATH/$X86_64_BIN" "$1"/linux/bin/"$X86_64_BIN"
-else
-  echo "$X86_64_BIN not found!"
-fi
+  if [ -f "$TARGET_PATH/$ARCH_PATH/$BIN" ]; then
+    git checkout $OS/$TARGET || git checkout -b $OS/$TARGET
+    if [ ! -d $OS/$ARCH_PATH ]; then
+      mkdir -p $OS/$ARCH_PATH
+    fi
+    cp -rf $TARGET_PATH/$ARCH_PATH/$BIN $OS/$ARCH_PATH/$BIN
+    git add .
+    git commit -m "$TAG_COMMIT"
+    git push origin $OS/$TARGET
+    git tag $TARGET"_$TAG_COMMIT"
+    git push --tags
+  else
+    echo "$TARGET not found!"
+  fi
+done
