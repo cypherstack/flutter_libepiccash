b43d37bff40e68aed6c9360e3270468b460237aa
julian <julian@cypherstack.com>
2023-04-23 07:43:09 -0600

    add tag exists check before attempting to commit tag

--- scripts/android/publish.sh
+++ scripts/android/publish.sh
@@ -17,20 +17,24 @@ BIN=libepic_cash_wallet.so
 
 for TARGET in arm64-v8a armeabi-v7a x86_64
 do
-  ARCH_PATH=$TARGET
-
-  if [ -f "$TARGET_PATH/$ARCH_PATH/$BIN" ]; then
-    git checkout $OS/$TARGET || git checkout -b $OS/$TARGET
-    if [ ! -d $OS/$ARCH_PATH ]; then
-      mkdir -p $OS/$ARCH_PATH
-    fi
-    cp -rf $TARGET_PATH/$ARCH_PATH/$BIN $OS/$ARCH_PATH/$BIN
-    git add .
-    git commit -m "$TAG_COMMIT"
-    git push origin $OS/$TARGET
-    git tag $TARGET"_$TAG_COMMIT"
-    git push --tags
+  if [ $(git tag -l "${TARGET}_${TAG_COMMIT}") ]; then
+    echo "Tag ${TARGET}_${TAG_COMMIT} already exists!"
   else
-    echo "$TARGET not found!"
+    ARCH_PATH=$TARGET
+
+    if [ -f "$TARGET_PATH/$ARCH_PATH/$BIN" ]; then
+      git checkout $OS/$TARGET || git checkout -b $OS/$TARGET
+      if [ ! -d $OS/$ARCH_PATH ]; then
+        mkdir -p $OS/$ARCH_PATH
+      fi
+      cp -rf $TARGET_PATH/$ARCH_PATH/$BIN $OS/$ARCH_PATH/$BIN
+      git add .
+      git commit -m "$TARGET commit for $TAG_COMMIT"
+      git push origin $OS/$TARGET
+      git tag $TARGET"_$TAG_COMMIT"
+      git push --tags
+    else
+      echo "$TARGET not found!"
+    fi
   fi
 done
--- scripts/ios/publish.sh
+++ scripts/ios/publish.sh
@@ -18,21 +18,25 @@ HEADER=libepic_cash_wallet.h
 
 for TARGET in aarch64-apple-ios x86_64-apple-ios
 do
-  ARCH_PATH=$TARGET/release
-
-  if [ -f "$TARGET_PATH/$ARCH_PATH/$BIN" ]; then
-    git checkout $OS/$TARGET || git checkout -b $OS/$TARGET
-    if [ ! -d $OS/$ARCH_PATH ]; then
-      mkdir -p $OS/$ARCH_PATH
-    fi
-    cp -rf $TARGET_PATH/$ARCH_PATH/$BIN $OS/$ARCH_PATH/$BIN
-    cp -rf $TARGET_PATH/../$HEADER $OS/$ARCH_PATH/$HEADER
-    git add .
-    git commit -m "$TAG_COMMIT"
-    git push origin $OS/$TARGET
-    git tag $TARGET"_$TAG_COMMIT"
-    git push --tags
+  if [ $(git tag -l "${TARGET}_${TAG_COMMIT}") ]; then
+    echo "Tag ${TARGET}_${TAG_COMMIT} already exists!"
   else
-    echo "$TARGET not found!"
+    ARCH_PATH=$TARGET/release
+
+    if [ -f "$TARGET_PATH/$ARCH_PATH/$BIN" ]; then
+      git checkout $OS/$TARGET || git checkout -b $OS/$TARGET
+      if [ ! -d $OS/$ARCH_PATH ]; then
+        mkdir -p $OS/$ARCH_PATH
+      fi
+      cp -rf $TARGET_PATH/$ARCH_PATH/$BIN $OS/$ARCH_PATH/$BIN
+      cp -rf $TARGET_PATH/../$HEADER $OS/$ARCH_PATH/$HEADER
+      git add .
+      git commit -m "$TARGET commit for $TAG_COMMIT"
+      git push origin $OS/$TARGET
+      git tag $TARGET"_$TAG_COMMIT"
+      git push --tags
+    else
+      echo "$TARGET not found!"
+    fi
   fi
 done
