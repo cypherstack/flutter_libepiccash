83daf84a17d9d6a21ed2c08bc74b11ea2ef69e11
sneurlax <sneurlax@gmail.com>
2023-04-24 14:18:58 -0500

    remove testing repo

--- scripts/android/download.sh
+++ scripts/android/download.sh
@@ -7,7 +7,7 @@ ANDROID_LIBS_DIR=$LIB_ROOT/android/src/main/jniLibs
 TAG_COMMIT=$(git log -1 --pretty=format:"%H")
 
 rm -rf flutter_libepiccash_bins
-git clone https://github.com/sneurlax/flutter_libepiccash_bins
+git clone https://git.cypherstack.com/stackwallet/flutter_libepiccash_bins
 if [ -d flutter_libepiccash_bins ]; then
   cd flutter_libepiccash_bins
 else
