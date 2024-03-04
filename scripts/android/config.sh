52f1ddfd33d130f8331ba2d34605ad5eec2382a3
likho <likhojiba@gmail.com>
2024-03-04 20:36:58 +0200

    Update config

--- scripts/android/config.sh
+++ scripts/android/config.sh
@@ -3,8 +3,8 @@
 export API=21
 export WORKDIR="$(pwd)/"build
 #r21e also works and was the preferred before
-export ANDROID_NDK_ZIP=${WORKDIR}/android-ndk-r20b-linux.zip
-export ANDROID_NDK_ROOT=${WORKDIR}/android-ndk-r21e
+export ANDROID_NDK_ZIP=${WORKDIR}/android-ndk-r25c-linux.zip
+export ANDROID_NDK_ROOT=${WORKDIR}/android-ndk-r25c
 export ANDROID_NDK_HOME=$ANDROID_NDK_ROOT
 
 
