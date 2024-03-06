3e3c0d21b0f6c3b06824e06622d4f98ff4fdd8a5
likho <likhojiba@gmail.com>
2024-03-06 15:43:02 +0200

    Fix path for git_versions.dart in the build script

--- scripts/android/build_all.sh
+++ scripts/android/build_all.sh
@@ -7,8 +7,8 @@ echo ${WORKDIR}
 
 rm -r ../../android/src/main/jniLibs/
 echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
-VERSIONS_FILE=../../lib/git_versions.dart
-EXAMPLE_VERSIONS_FILE=../../lib/git_versions_example.dart
+VERSIONS_FILE=../../../lib/git_versions.dart
+EXAMPLE_VERSIONS_FILE=../../../lib/git_versions_example.dart
 if [ ! -f "$VERSIONS_FILE" ]; then
     cp $EXAMPLE_VERSIONS_FILE $VERSIONS_FILE
 fi
