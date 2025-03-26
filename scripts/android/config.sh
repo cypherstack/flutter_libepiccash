0796f5caa988d128fa84faf1854cb857f2bb6f5c
sneurlax <sneurlax@gmail.com>
2025-03-26 12:57:38 -0500

    feat: use ndkVersion 28.0.13004108 in android build config

--- scripts/android/config.sh
+++ scripts/android/config.sh
@@ -2,9 +2,9 @@
 
 export WORKDIR="$(pwd)/"build
 export CACHEDIR="$(pwd)/"cache
-ANDROID_NDK_API=25c
+ANDROID_NDK_API=28
 
-export ANDROID_NDK_SHA256="769ee342ea75f80619d985c2da990c48b3d8eaf45f48783a2d48870d04b46108"
+export ANDROID_NDK_SHA256="a186b67e8810cb949514925e4f7a2255548fb55f5e9b0824a6430d012c1b695b"
 export ANDROID_NDK_URL=https://dl.google.com/android/repository/android-ndk-r${ANDROID_NDK_API}-linux.zip
 # Some NDK versions end in -linux.zip, some in -linux_x86_64.zip.
 export ANDROID_NDK_ZIP=${CACHEDIR}/android-ndk-r${ANDROID_NDK_API}-linux.zip
