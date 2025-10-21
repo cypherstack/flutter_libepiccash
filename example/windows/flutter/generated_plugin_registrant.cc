837c91858fb5d438c3f6ea6311e695f49974e5ed
sneurlax <sneurlax@gmail.com>
2025-10-20 22:10:09 -0500

    feat: remove unused MethodChannel based plugin scaffolding
    and remove old MethodChannel-related generated and/or platform-specific code

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_libepiccash/flutter_libepiccash_plugin_c_api.h>
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterLibepiccashPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterLibepiccashPluginCApi"));
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,11 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  flutter_libepiccash
   flutter_secure_storage_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_libepiccash
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
