779d8dde738e7a290fc12ba699a6ab7bb0bc9c1f
sneurlax <sneurlax@gmail.com>
2026-01-09 15:57:21 -0600

    fix: epicbox message processing

--- rust/src/listener.rs
+++ rust/src/listener.rs
@@ -14,7 +14,6 @@ use crate::wallet::Wallet;
 #[derive(Debug, Clone)]
 pub struct Listener {
     pub wallet_ptr_str: String,
-    // pub wallet_data: (i64, Option<SecretKey>),
     pub epicbox_config: String
 }
 
@@ -26,30 +25,36 @@ impl Task for Listener {
         let mut spins = 0;
 
         let wallet_data_str = &self.wallet_ptr_str;
-        // let wallet_data = wallet_ptr.to_str().unwrap();
         let tuple_wallet_data: (i64, Option<SecretKey>) = serde_json::from_str(wallet_data_str).unwrap();
         let wlt = tuple_wallet_data.0;
         let sek_key = tuple_wallet_data.1;
 
-        // let wallet_data = &self.wallet_data;
-        // let wlt = wallet_data.clone().0;
         unsafe {
             let epicbox_conf = serde_json::from_str::<EpicboxConfig>(&self.epicbox_config.as_str()).unwrap();
-            // let wallet_data = &self.wallet_data;
-            // let wlt = wallet_data.0;
-            // let sek_key = wallet_data.clone().1;
+
             crate::ensure_wallet!(wlt, wallet);
+
             while !cancel_tok.cancelled() {
                 let listener = EpicboxListenChannel::new().unwrap();
+
                 let mut reconnections = 0;
-                listener.listen(
+
+                match listener.listen(
                     wallet.clone(),
                     Arc::new(Mutex::new(sek_key.clone())),
                     epicbox_conf.clone(),
                     &mut reconnections,
-                    StdArc::new(AtomicBool::new(false)),
+                    // IMPORTANT: Must be true for the listener to process messages.
+                    // When false, the epicbox subscriber loop just sleeps and skips message processing.
+                    StdArc::new(AtomicBool::new(true)),
                     TorConfig::default(),
-                ).expect("TODO: Error Listening on Epicbox");
+                ) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        return Err(anyhow::Error::msg(format!("Epicbox listener error: {}", e)));
+                    }
+                }
+
                 spins += 1;
             }
         }
