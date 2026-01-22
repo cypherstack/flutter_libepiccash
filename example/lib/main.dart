74306c2a2a738439f874972bc1ccd32cf4d94d70
sneurlax <sneurlax@gmail.com>
2026-01-22 14:50:34 -0600

    feat: log more example app errors

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,55 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'wallet_management_view.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  // Catch Flutter framework errors
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    developer.log(
+      'Flutter Error',
+      name: 'flutter_libepiccash_example',
+      error: details.exception,
+      stackTrace: details.stack,
+    );
+    print('Exception: ${details.exception}');
+    print('Stack trace:\n${details.stack}');
+  };
+
+  // Catch async errors
+  PlatformDispatcher.instance.onError = (error, stack) {
+    developer.log(
+      'Async Error',
+      name: 'flutter_libepiccash_example',
+      error: error,
+      stackTrace: stack,
+    );
+    print('Error: $error');
+    print('Stack trace:\n$stack');
+    return true;
+  };
+
+  // Run app with error handling
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(const MyApp());
+    },
+    (error, stack) {
+      developer.log(
+        'Zone Error',
+        name: 'flutter_libepiccash_example',
+        error: error,
+        stackTrace: stack,
+      );
+      print('Error: $error');
+      print('Stack trace:\n$stack');
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
